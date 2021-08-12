c4c619f740bf47166fdab89df38a89f1165f2a3f
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-12 20:05:02 +0900

    入力した値を未完了リストに追加する機能を生成

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@ import { React, useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  //入力した値をState化する
+  const [todoText, setTodoText] = useState("");
   //未完了のTODOを格納する配列を作る
   const [incompleteTodos, setIncompleteTodos] = useState([
     "今日のタスク",
     "今日のタスク２"
   ]);
   const [completeTodos, setCompleteTodos] = useState(["今日のタスク３"]);
+  //onChangeの関数
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    //入力した値を未完了リストに追加する配列
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    // alert(todoText);
+    setTodoText("");
+  };
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
