7f86b7e7e299858909057ff786fd3b9a2e3dc83a
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-13 13:11:07 +0900

    コンポーネント化を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { React, useState } from "react";
 import "./styles.css";
+import { InputTodo } from "./Components/inputTodoo";
+import { IncompleteTodos } from "./Components/incompleteTodos";
+import { CompleteTodos } from "./Components/completeTodos";
 
 export const App = () => {
   //入力した値をState化する
@@ -47,41 +50,17 @@ export const App = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeTodoText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickcomplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="complete-area">
-        <p className="title">完了のTODO</p>
-        <ul>
-          {completeTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickcomplete={onClickcomplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
     </>
   );
 };
